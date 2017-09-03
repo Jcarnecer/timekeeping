48fe840e77e6eada600369513e84a586a1c30f70
Erin Rugas <erinjohnrugas@gmail.com>
2017-09-04 01:05:56 +0800

    updated sql

--- timekeeping_sirjun.sql
+++ timekeeping_sirjun.sql
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS `employee`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_number` varchar(255) DEFAULT NULL,
+  `employee_number` varchar(20) DEFAULT NULL,
   `sss_no` int(11) DEFAULT NULL,
   `tin_no` int(11) DEFAULT NULL,
   `phil_health` int(11) DEFAULT NULL,
   `start_date` date DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'1',1234,1234,1234,'0000-00-00',14),(2,'1',12345,12345,12345,'0000-00-00',15),(3,'1',12345,12345,12345,'0000-00-00',17);
+INSERT INTO `employee` VALUES (1,NULL,1234,NULL,NULL,NULL,2);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,8 +60,9 @@ CREATE TABLE `intern` (
   `birthday` date DEFAULT NULL,
   `year` varchar(50) DEFAULT NULL,
   `start_date` date DEFAULT NULL,
+  `remaining` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +71,7 @@ CREATE TABLE `intern` (
 
 LOCK TABLES `intern` WRITE;
 /*!40000 ALTER TABLE `intern` DISABLE KEYS */;
-INSERT INTO `intern` VALUES (1,12,'Agin',222,'Asdadas',NULL,'1','2017-01-10'),(2,13,'Asd',520,'Asd',NULL,'2017-2016','2017-01-10'),(3,16,'Zxcc',520,'Zxcz',NULL,'1','0000-00-00');
+INSERT INTO `intern` VALUES (1,3,'UST',250,'BSITWMA','2017-05-06','2017-2018','2016-01-05','250');
 /*!40000 ALTER TABLE `intern` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -130,7 +131,7 @@ CREATE TABLE `menu` (
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,'Dashboard','fa fa-dashboard m-r-10',1,'2017-08-21 14:10:21','0000-00-00 00:00:00','dashboard',NULL),(2,'User Management','fa fa-users m-r-10',1,'2017-08-21 14:10:21','2017-08-25 01:04:06','users',NULL),(3,'Attendance','fa fa-calendar m-r-10',1,'2017-08-21 14:10:21','0000-00-00 00:00:00','',NULL),(4,'Management Shift','fa fa-clock-o m-r-10',1,'2017-08-21 14:10:21','0000-00-00 00:00:00','',NULL),(5,'Role Management','fa fa-sitemap m-r-10',1,'2017-08-21 14:10:21','0000-00-00 00:00:00','role',NULL);
+INSERT INTO `menu` VALUES (1,'Dashboard','fa fa-dashboard',1,'2017-08-21 14:10:21','2017-09-02 10:03:46','dashboard',NULL),(2,'User Management','fa fa-users',1,'2017-08-21 14:10:21','2017-09-02 10:03:58','users',NULL),(3,'Attendance','fa fa-calendar',1,'2017-08-21 14:10:21','2017-09-02 10:04:06','',NULL),(4,'Management Shift','fa fa-clock',1,'2017-08-21 14:10:21','2017-09-02 10:07:33','',1),(5,'Position Management','fa fa-sitemap',1,'2017-08-21 14:10:21','2017-09-02 10:04:25','position',NULL);
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,7 +149,7 @@ CREATE TABLE `position` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,7 +158,7 @@ CREATE TABLE `position` (
 
 LOCK TABLES `position` WRITE;
 /*!40000 ALTER TABLE `position` DISABLE KEYS */;
-INSERT INTO `position` VALUES (1,'Admin','1,2,3,4,5','2017-08-22 02:02:42','2017-08-22 03:15:22'),(2,'Employee','1,3','2017-08-22 02:02:55','2017-09-01 04:49:03'),(3,'Human Resource','1,2,4,5','2017-08-22 05:02:01','2017-09-01 04:49:12'),(4,'Intern','1,3','2017-08-22 05:18:24','0000-00-00 00:00:00');
+INSERT INTO `position` VALUES (1,'Admin','1,2,3,4,5','2017-08-22 02:02:42','2017-08-22 03:15:22'),(2,'Employee','1,3','2017-08-22 02:02:55','2017-09-01 05:39:44'),(3,'Human Resource','1,2,4,5','2017-08-22 05:02:01','2017-09-01 04:49:12'),(4,'Intern','1,3','2017-08-22 05:18:24','0000-00-00 00:00:00'),(11,'Manager','1,2,3,4,5','2017-09-01 05:40:00','0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `position` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,6 +218,32 @@ INSERT INTO `school` VALUES (1,'UST',1,250);
 UNLOCK TABLES;
 
 --
+-- Table structure for table `shift`
+--
+
+DROP TABLE IF EXISTS `shift`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shift` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shift_type` varchar(50) DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shift`
+--
+
+LOCK TABLES `shift` WRITE;
+/*!40000 ALTER TABLE `shift` DISABLE KEYS */;
+INSERT INTO `shift` VALUES (1,'Morning','08:00:00','17:00:00'),(2,'Mid','15:00:00','01:00:00'),(3,'Night','22:00:00','09:00:00');
+/*!40000 ALTER TABLE `shift` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `sub_menu`
 --
 
@@ -229,7 +256,7 @@ CREATE TABLE `sub_menu` (
   `url` varchar(100) NOT NULL,
   `menu_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -238,6 +265,7 @@ CREATE TABLE `sub_menu` (
 
 LOCK TABLES `sub_menu` WRITE;
 /*!40000 ALTER TABLE `sub_menu` DISABLE KEYS */;
+INSERT INTO `sub_menu` VALUES (1,'Shift Type','shift',4),(2,'Shift Requests','requests',4);
 /*!40000 ALTER TABLE `sub_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -273,7 +301,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Erin','Rugas','','ejr012495@gmail.com','$2y$11$XMwYhWyiWjLuObLiR6fewOqQfbMvIGPYgCJRlHEoKE0oKiEYUJjAq','no_image.jpg',1,NULL,1,'_9aJFDqEc4L_',0,'2017-08-30 12:19:46','2017-08-31 02:45:47'),(2,'Asd','Asd','Asd','asd@mail.com','$2y$11$UUiOr0HX5G9vehGtqaI/wuxjFp1sstBXvJxt6U06FzfQFGggtdKFW','no_image.jpg',3,NULL,1,'_LzGBmgDY43_',0,'2017-08-30 12:35:24','2017-09-01 03:11:29'),(3,'Abc','Abc','Abc','abc@mail.com','$2y$11$lN0qTwD7zRe226uwJXuH/udA/L4yHVszm0QtXXeBV6Dy1mRuvZWf.','no_image.jpg',2,NULL,1,'_aAQ4xNVBPj_',0,'2017-08-30 13:32:15','2017-08-31 03:33:30');
+INSERT INTO `users` VALUES (1,'Erin','Rugas','','ejr012495@gmail.com','$2y$11$Kt4z2p0tCAHgSarRHqEWXu7rmWOh46xpH9ygp2nK8pdEWMYwmTbsS','no_image.jpg',1,NULL,1,'_Wix4k7mYT2_',0,'2017-09-03 12:21:05','2017-09-03 12:21:49'),(2,'Employee','Employee','','employee@mail.com','$2y$11$NVZNbD4tTOk4Cs6Go.trMe8ffOTt4CHlTHEPWCeOYctetMvwJXkea','6b48c0b21b979322e0cabb9bc38a081d.jpg',2,NULL,1,'_RIO8Qx0hwU_',0,'2017-09-03 12:22:39','2017-09-03 16:33:05'),(3,'Intern','Intern','','intern@mail.com','$2y$11$W1brU2Inx.5nLAOr4khcweCh/3pttEI3hSICpuojIF1h6Cy7Y8PtG','c86f2b763e9443c28255ccb39e858917.jpeg',4,NULL,1,'_v98lGqSxng_',0,'2017-09-03 12:24:12','2017-09-03 16:43:33');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -286,4 +314,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-01 12:54:24
+-- Dump completed on 2017-09-04  1:05:39
