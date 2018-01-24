8092c42f7b06dd79c175fce0470a16f14a2565eb
martslizardo <antoniomartin.lizardo@yahoo.com>
2018-01-24 14:26:46 +0800

    Fixed Validation

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -153,8 +153,8 @@ $config =
                 [
                     'field'=>'leave_name',
                     'label'=>'Leave',
-                    'rules'=>'required|is_unique[timekeeping_leave.leave_name]|regex_match[/^([a-zA-Z0-9@.,_]|\s)+$/]',
-                    'errors'=> ['is_unique'=>'%s is already taken',],
+                    'rules'=>'required|regex_match[/^([a-zA-Z0-9@.,_]|\s)+$/]',
+                    
                   ],
                   [
                     'field'=>'days',
