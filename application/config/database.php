6b5cd6ac1793ee7ab184ed488369d827d7cba5e4
martslizardo <antoniomartin.lizardo@yahoo.com>
2018-02-17 15:32:16 +0800

    added enviroment(production)

--- application/config/database.php
+++ application/config/database.php
@@ -73,6 +73,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+$active_group = 'default';
+$query_builder = TRUE;
+
+if (ENVIRONMENT === "production") {
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'smallappsdatabase.ct6zpeav3tx4.ap-southeast-1.rds.amazonaws.com',
+		'username' => 'astrid',
+		'password' => 'astridtechnologies',
+		'database' => 'small_apps',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => FALSE,
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+	);	
+}else{
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
@@ -93,4 +119,5 @@ $db['default'] = array(
 	'stricton' => FALSE,	
 	'failover' => array(),
 	'save_queries' => TRUE
-);
+	);
+}
