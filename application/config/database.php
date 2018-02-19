445749af415992ea2f038e1aa20360d2cb8a7f7a
martslizardo <antoniomartin.lizardo@yahoo.com>
2018-02-19 09:12:16 +0800

    edit database config

--- application/config/database.php
+++ application/config/database.php
@@ -76,6 +76,7 @@ $query_builder = TRUE;
 $active_group = 'default';
 $query_builder = TRUE;
 
+
 if (ENVIRONMENT === "production") {
 	$db['default'] = array(
 		'dsn'	=> '',
@@ -98,26 +99,27 @@ if (ENVIRONMENT === "production") {
 		'failover' => array(),
 		'save_queries' => TRUE
 	);	
-}else{
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'small_apps',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => TRUE,
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,	
-	'failover' => array(),
-	'save_queries' => TRUE
+} else {
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'small_apps',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => TRUE,
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
 	);
 }
+
